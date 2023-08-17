f743980f6c551474256331d0df90adb7522162e9
Long Nguyễn Giao Huỳnh <pierre@pierreneter.com>
2023-08-17 17:31:37 +0700

    setup deploy staging client

--- src/services/smc.ts
+++ src/services/smc.ts
@@ -1,12 +1,12 @@
 import * as ethers from "ethers";
 import abi from "./abi.json";
 const provider = new ethers.providers.JsonRpcProvider(
-  process.env.REACT_APP_RPC_HTTPS!
+  process.env.REACT_APP_RPC_HTTP!
 );
 
 console.log(
   "7s1:",
-  process.env.REACT_APP_RPC_HTTPS!,
+  process.env.REACT_APP_RPC_HTTP!,
   process.env.REACT_APP_NFT_CONTRACT_ADDRESS!
 );
 export const balueSMC = new ethers.Contract(
